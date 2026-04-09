b38368f195587a088cd91b1529d1fc59bf3e7120
maloltzahi-byte <maloltzahi@gmail.com>
2026-04-09 13:15:38 +0300

    Delete app.config.js

--- app.config.js
+++
@@ -1,9 +0,0 @@
-window.APP_CONFIG = {
-  // חיבור למסד הנתונים שלך ב-Supabase
-  SUPABASE_URL: 'https://lcvshepgzizrlqbzvjoe.supabase.co',
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImxjdnNoZXBneml6cmxxYnp2am9lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU0MDU5NzEsImV4cCI6MjA5MDk4MTk3MX0.ca4rw8QeRoPdQPwAsqPiLEZD6S-eOCdw6-P4a4BIFNk',
-  
-  // הגדרות ניתוב לוואטסאפ
-  WHATSAPP_NUMBER: '0501234567',
-  WHATSAPP_MESSAGE: 'שלום, הגעתי דרך המערכת לבדיקת זכאות משפטית לרישיון כלי ירייה.'
-};
